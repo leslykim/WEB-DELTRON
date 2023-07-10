9a950cc0832813e1a0b55792362602abdcb67035
leslykim <42583853+leslykim@users.noreply.github.com>
2023-07-10 00:40:20 -0500

    Add files via upload

---
+++ deltron-landing/deltron-landing/assets/js/data.js
@@ -0,0 +1,155 @@
+const categories = [
+  {
+    name: 'Computadoras',
+    total: 35,
+    img: 'assets/images/categories/car.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Discos Duros',
+    total: 22,
+    img: 'assets/images/categories/laptop.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Monitores',
+    total: 55,
+    img: 'assets/images/categories/matrimony.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Impresoras',
+    total: 21,
+    img: 'assets/images/categories/furniture.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Memorias RAM',
+    total: 44,
+    img: 'assets/images/categories/jobs.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Memorias Flash',
+    total: 65,
+    img: 'assets/images/categories/real-estate.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Tablet',
+    total: 35,
+    img: 'assets/images/categories/laptop.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'CPU',
+    total: 22,
+    img: 'assets/images/categories/hospital.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Tarjetas de video / dispositivos de video',
+    total: 25,
+    img: 'assets/images/categories/tshirt.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Cdrom / dvd / blu ray drives',
+    total: 42,
+    img: 'assets/images/categories/education.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Suministros',
+    total: 32,
+    img: 'assets/images/categories/controller.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Cases',
+    total: 15,
+    img: 'assets/images/categories/travel.svg',
+    href: 'category.html',
+  },
+  {
+    name: 'Motherboards',
+    total: 65,
+    img: 'assets/images/categories/watch.svg',
+    href: 'category.html',
+  },
+];
+
+const last_products = [
+  {
+    brand: 'Apple',
+    brand_img:  'assets/images/items-grid/author-2.jpg',
+    category: 'Apple',
+    name: 'Apple Iphone X',
+    img: 'assets/images/items-grid/img2.jpg',
+    star_point: 135,
+    price: 4500.00,
+    is_wish: false,
+    last_update: 'Hace 1 hora'
+  },
+  {
+    brand: 'ASUS',
+    brand_img:  'assets/images/items-grid/author-2.jpg',
+    category: 'ASUS',
+    name: 'ROG Strix B660-G Gaming',
+    img: 'assets/images/items-grid/img2.jpg',
+    star_point: 550,
+    price: 2700,
+    is_wish: true,
+    last_update: 'Hace 2 hora'
+  },
+  {
+    brand: 'Canon',
+    brand_img:  'assets/images/items-grid/author-2.jpg',
+    category: 'Canon',
+    name: 'Multifuncional Pixma Serie G',
+    img: 'assets/images/items-grid/img2.jpg',
+    star_point: 120,
+    price: 1000.00,
+    is_wish: false,
+    last_update: 'Hace 3 hora'
+  },
+  {
+    brand: 'Apple',
+    brand_img:  'assets/images/items-grid/author-2.jpg',
+    category: 'Apple',
+    name: 'Apple Macbook Pro 13 Inch',
+    img: 'assets/images/items-grid/img2.jpg',
+    star_point: 135,
+    price: 2500.00,
+    is_wish: false,
+    last_update: 'Hace 4 hora'
+  },
+]
+
+const cities = [
+  {
+    name: 'Lima',
+    detail: 'Ciberplaza y Miraflores',
+    img: 'assets/images/local/Lima.webp'
+  },
+  {
+    name: 'Arequipa',
+    detail: 'Ciberplaza y Cerro Colorado',
+    img: 'assets/images/local/Arequipa.webp'
+  },
+  {
+    name: 'Chiclayo',
+    detail: 'Centro Civico',
+    img: 'assets/images/local/Chiclayo.webp'
+  },
+  {
+    name: 'Cusco',
+    detail: 'Plaza de Armas',
+    img: 'assets/images/local/Cusco.webp'
+  },
+  {
+    name: 'Huancayo',
+    detail: 'San Lazaro',
+    img: 'assets/images/local/Huancayo.webp'
+  }
+]
